bb6a29ad4046c0befc6e5ff8a6edc376d2b935cb
ahmad <ahmad2smile@gmail.com>
2018-11-15 16:14:59 +0500

    fixed -js not working release 0.2

--- src/arguments.rs
+++ src/arguments.rs
@@ -5,19 +5,19 @@ pub struct Arguments {
 
 impl Arguments {
 	pub fn new(args: &[String]) -> Result<Arguments, &'static str> {
-		let help_message = "Usage: duckName1 duckNam2[ -j]
-			\r\n -j for JS files, default is TS
+		let help_message = "Usage: duckName1 duckNam2[ -js]
+			\r\n -js for JS files, default is TS
 			\r\n -h --help to show this message";
 
 		if args.len() < 1 {
 			println!("{}", help_message);
 
 			return Err("Please provide a DUCK name!");
-		} else if args.clone().iter().any(|x| x == "-j") {
+		} else if args.clone().iter().any(|x| x == "-js") {
 			return Ok(Arguments {
 				duck: args
 					.iter()
-					.filter(|arg| arg.contains("-js"))
+					.filter(|arg| !arg.contains("-js"))
 					.cloned()
 					.collect(),
 				extension: ".js".to_string(),
